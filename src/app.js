b02fc505fe22b762382810849a8bb205451e4f2c
Fallen Angel <speakofthedevil37@gmail.com>
2025-08-04 16:43:27 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+// src/app.js
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from public
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
